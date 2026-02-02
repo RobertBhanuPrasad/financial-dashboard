3c582828162253a9c96f114f4c0937809ef2730e
Robert Bhanu Prasad <bhanu.robert@artofliving.org>
2026-02-02 21:31:26 +0530

    feat:pagination and submition of the form is added

--- nextjs-dashboard/app/lib/action.ts
+++ nextjs-dashboard/app/lib/action.ts
@@ -1,5 +1,22 @@
 'use server';
+import {z} from "zod"
+
+const FormSchema = z.object({
+    id: z.string(),
+    customerId: z.string(),
+    amount: z.coerce.number(),
+    status: z.enum(['pending', 'paid']),
+    date: z.string(),
+})
+
+const createInvoice = FormSchema.omit({id: true, date: true})
 
 export default async function createInvoices(formData: FormData) {
-    
+    const {customerId, amount, status} = createInvoice.parse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    });
+    // Test it out:
+    console.log(customerId, amount, status, "bhanuformdata");
 }
